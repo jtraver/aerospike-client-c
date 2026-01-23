3feedc1ab1124d78506641a654252b11b0037df5
John Traver <john@aerospike.com>
2026-01-23 10:58:01 -0700

    update

--- john/disable_upstream_push.sh
+++ john/disable_upstream_push.sh
@@ -1,3 +1,12 @@
 git remote -v
-git remote set-url --push upstream ''
+# git remote set-url --push upstream ''
+git remote set-url --push upstream DISABLE
+# jtarver@aeroadmin-MBP-NMWG9 aerospike-client-c % git remote set-url --push upstream DISABLE
+# jtarver@aeroadmin-MBP-NMWG9 aerospike-client-c % git remote -v
+# origin	git@github.com:jtraver/aerospike-client-c.git (fetch)
+# origin	git@github.com:jtraver/aerospike-client-c.git (push)
+# upstream	git@github.com:aerospike/aerospike-client-c.git (fetch)
+# upstream	DISABLE (push)
+# jtarver@aeroadmin-MBP-NMWG9 aerospike-client-c %
+
 git remote -v
